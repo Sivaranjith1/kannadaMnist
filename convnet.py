8bb154b9a36b79e0c17d580bf980f812deb8d11b
Ranjith siva <ranjith1210000@gmail.com>
2019-11-18 08:43:40 +0100

    model

---
+++ convnet.py
@@ -0,0 +1,40 @@
+# %%
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+
+# %%
+NAME = "Kannada-Mnist"
+
+# %%
+X = pickle.load(open('X.pickle', 'rb'))
+Y = pickle.load(open('Y.pickle', 'rb'))
+
+X[0]
+# %%
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(64, (2, 2), input_shape=(28, 28, 1)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (2, 2)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(10))
+model.add(Activation("softmax"))
+
+model.compile(
+    loss="categorical_crossentropy",
+    optimizer="adam",
+    metrics=['accuracy']
+)
+
+model.fit(X, Y, batch_size=40, validation_split=0.1, epochs=10)
+
+# %%
--- dataPreprossessing.py
+++ dataPreprossessing.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import pickle
+from sklearn.preprocessing import OneHotEncoder
 
 filname = 'dataset/train.csv'
 x = []
@@ -19,12 +20,16 @@ with open(filname) as csvFile:
             x.append(x_in)
         n += 1
 
-print('len(x): ', len(x), 'len(x[0]): ', len(x[0]), "y[56]: ", y[56])
+
+X = np.asarray(x)
+X = np.reshape(X, (len(x), 28, -1, 1))
 
 Y = np.asarray(y)
-X = np.asarray(x)
-X = np.reshape(X, (len(x), 28, -1))
-
+Y = np.reshape(Y, (-1, 1))
+onehotencoder = OneHotEncoder()
+Y = onehotencoder.fit_transform(Y).toarray()
+print('len(x): ', len(x), 'len(x[0]): ', len(
+    x[0]), "y[56]: ", y[56], "Y[56]: ", Y[56])
 print(X.shape)
 
 pickle_out = open("X.pickle", "wb")
